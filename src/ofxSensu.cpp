8ecaa38ed96b9eb0c6d00b11ba4436bc2efd8bf1
nicoleyimessier <nicolemessier@localprojects.com>
2018-01-19 11:17:22 -0500

    updating ofxsensu for keep alive

--- src/ofxSensu.cpp
+++ src/ofxSensu.cpp
@@ -15,13 +15,13 @@ ofxSensu::~ofxSensu(){
 
 
 void ofxSensu::setup(string ip, int port) {
-
+	
 	if (isSetup) {
-
+		
 		ofLogError("ofxSensu") << "Already setup! dont setup twice!";
-
+		
 	}else{
-
+		
 		ofLogNotice("ofxSensu") << "Setting up with host: " << ip << " port: " << port;
 		settings.address = ip;
 		settings.port = port;
@@ -32,12 +32,12 @@ void ofxSensu::setup(string ip, int port) {
 }
 
 bool ofxSensu::isServerThere(){
-
+	
 	if(!isSetup){
 		ofLogError("ofxSensu") << "not setup! set me up first!";
 		return false;
 	}
-
+	
 	ofxTCPClient tempClient;
 	bool ok = tempClient.setup(settings);
 	bool ret = false;
@@ -64,10 +64,10 @@ bool ofxSensu::send(const string & alertName,
 					const string & filePath,
 					bool blocking
 					){
-
+	
 	vector<string> files;
 	if(filePath.size()) files.push_back(filePath);
-
+	
 	return send(alertName, msg, status, emailAddresses, files, blocking);
 }
 
@@ -79,22 +79,22 @@ bool ofxSensu::send(const string & alertName,
 					const vector<string> & filePaths,
 					bool blocking
 					){
-
+	
 	if(!isSetup){
 		ofLogError("ofxSensu") << "not setup! set me up first!";
 		return false;
 	}
-
+	
 	if(emailAddresses.size() == 0){
 		ofLogError("ofxSensu") << "can't send command without an email address!";
 		return false;
 	}
-
+	
 	ofLogNotice("ofxSensu") << 	"sending Alert: \"" << alertName << "\" msg: \"" << msg << "\" to " <<
-								emailAddresses.size() << " email addresses with " << filePaths.size() << " files.";
-
+	emailAddresses.size() << " email addresses with " << filePaths.size() << " files.";
+	
 	string command = buildCommand(alertName, msg, status, emailAddresses, filePaths);
-
+	
 	if(blocking){ //lets connect right
 		return sendCmdBlocking(command);
 	}else{
@@ -109,41 +109,41 @@ bool ofxSensu::send(const string & alertName,
 }
 
 void ofxSensu::buildStillAliveMessage(const string & name,
-                            const int ttl,
-                            const string & output,
-                            const string & remediation,
-                            const int occurrences,
-                            const int severity){
-
-    const char* format = "{"
-        "\"name\": \"%s\", "
-        "\"ttl\": %d, "
-        "\"handlers\": [\"remediator\"], "
-        "\"output\":\"%s\", "
-        "\"remediation\": { "
-                        "\"%s\": "
-                            "{ "
-                             "\"occurrences\" : [%d], "
-                             "\"severities\" : [%d] "
-                           "}}}";
-
-    auto size = std::snprintf(nullptr, 0, format,
-                                name.c_str(),
-                                ttl,
-                                output.c_str(),
-                                remediation.c_str(),
-                                occurrences,
-                                severity
-                             );
-    stillAliveMsg = string(size + 1, '\0');
-    std::sprintf(&stillAliveMsg[0], format,
-                                name.c_str(),
-                                ttl,
-                                output.c_str(),
-                                remediation.c_str(),
-                                occurrences,
-                                severity
-                            );
+									  const int ttl,
+									  const string & output,
+									  const string & remediation,
+									  const int occurrences,
+									  const int severity){
+	
+	const char* format = "{"
+	"\"name\": \"%s\", "
+	"\"ttl\": %d, "
+	"\"handlers\": [\"remediator\"], "
+	"\"output\":\"%s\", "
+	"\"remediation\": { "
+	"\"%s\": "
+	"{ "
+	"\"occurrences\" : [%d], "
+	"\"severities\" : [%d] "
+	"}}}";
+	
+	auto size = std::snprintf(nullptr, 0, format,
+							  name.c_str(),
+							  ttl,
+							  output.c_str(),
+							  remediation.c_str(),
+							  occurrences,
+							  severity
+							  );
+	stillAliveMsg = string(size + 1, '\0');
+	std::sprintf(&stillAliveMsg[0], format,
+				 name.c_str(),
+				 ttl,
+				 output.c_str(),
+				 remediation.c_str(),
+				 occurrences,
+				 severity
+				 );
 }
 
 string ofxSensu::buildCommand(const string & alertName,
@@ -151,68 +151,68 @@ string ofxSensu::buildCommand(const string & alertName,
 							  Status status,
 							  const vector<string> & emailAddresses,
 							  const vector<string> & filePaths){
-
+	
 	string emailList;
 	for(auto & s : emailAddresses){
 		emailList += "\"" + s + "\", ";
 	}
 	if(emailAddresses.size()) emailList = emailList.substr(0, emailList.size()-2); //remove last comma
-
+	
 	string fileList;
 	for(auto & s : filePaths){
 		string p = ofToDataPath(s, true);
-		#ifdef TARGET_WIN32
+#ifdef TARGET_WIN32
 		ofStringReplace(p, "\\", "/");
-		#endif
+#endif
 		fileList += "\"" + p + "\", ";
 	}
 	if(fileList.size()) fileList = fileList.substr(0, fileList.size()-2); //remove last comma
-
+	
 	if(status < RESOLVED) status = RESOLVED;
 	if(status > CRITICAL_ERROR) status = CRITICAL_ERROR;
-
+	
 	string cleanMsg = msg;
 	ofStringReplace(cleanMsg, "\"", "\\\""); //escape quotes for json to be valid
 	ofStringReplace(cleanMsg, "\n", "\\n"); //escape newlines for json to be valid
-
-
-
+	
+	
+	
 	string command = 	"{\"name\" : \"" + alertName + "\", " +
-						"\"output\" : \"" + cleanMsg + "\", " +
-						"\"status\" : " + ofToString((int)status) + ", " +
-						"\"handlers\" : [\"email\"], " +
-						"\"email_to\" : [" + emailList + "], " +
-						"\"files\": [" + fileList + "], " +
-						"\"occurrences\": 1, " +
-						"\"refresh\": 60 " +
-						"}";
+	"\"output\" : \"" + cleanMsg + "\", " +
+	"\"status\" : " + ofToString((int)status) + ", " +
+	"\"handlers\" : [\"email\"], " +
+	"\"email_to\" : [" + emailList + "], " +
+	"\"files\": [" + fileList + "], " +
+	"\"occurrences\": 1, " +
+	"\"refresh\": 60 " +
+	"}";
 	return command;
 }
 
 void ofxSensu::sendStillAlive(){
-    
-    if(!isSetup){
-        ofLogError("ofxSensu") << "not setup! set me up first!";
-        return;
-    }
-    if(stillAliveMsg.length() == 0){
-        ofLogError("ofxSensu") << "No still alive message specified, call buildStillAliveMessage first!";
-        return;
-    }
-
-    lock();
-    commands.push_back(stillAliveMsg);
-    unlock();
-    if(!isThreadRunning()){
-        startThread();
-    }
+	
+	if(!isSetup){
+		ofLogError("ofxSensu") << "not setup! set me up first!";
+		return;
+	}
+	if(stillAliveMsg.length() == 0){
+		ofLogError("ofxSensu") << "No still alive message specified, call buildStillAliveMessage first!";
+		return;
+	}
+	
+	lock();
+	commands.push_back(stillAliveMsg);
+	unlock();
+	if(!isThreadRunning()){
+		startThread();
+	}
 }
 
 bool ofxSensu::connect(ofxTCPClient & client){
-
+	
 	bool connected = client.setup(settings);
 	int numFail = 0;
-
+	
 	while(!connected && isThreadRunning()){ //try reconnect if not connected
 		numFail++;
 		ofLogWarning("ofxSensu") 	<< "can't connect to Sensu Server at \"" << settings.address << ":" << settings.port
@@ -229,26 +229,26 @@ bool ofxSensu::connect(ofxTCPClient & client){
 
 
 void ofxSensu::threadedFunction(){
-
-	#ifdef TARGET_WIN32
-	#elif defined(TARGET_LINUX)
+	
+#ifdef TARGET_WIN32
+#elif defined(TARGET_LINUX)
 	pthread_setname_np(pthread_self(), "ofxSensu");
-	#else
+#else
 	pthread_setname_np("ofxSensu");
-	#endif
-
+#endif
+	
 	int nCommands = 0;
-
+	
 	lock();
 	nCommands = commands.size();
 	unlock();
-
+	
 	//at this point we should be connected
 	while(nCommands > 0){
-
+		
 		ofxTCPClient client; //each new command starts a new TCP connection!
 		bool connected = connect(client);
-
+		
 		if(!connected){ //this will only happen if we are trying to stop the thread
 			return; //otherwise connect will keep trying forever
 		}
@@ -256,15 +256,15 @@ void ofxSensu::threadedFunction(){
 		string cmd = commands.front();
 		commands.erase(commands.begin());
 		unlock();
-
+		
 		bool sent = client.send(cmd);
 		ofSleepMillis(1);
 		string rec = client.receiveRaw();
 		client.close();
-
+		
 		ofLogNotice("ofxSensu") << "command sent '" << cmd << "'";
 		ofLogNotice("ofxSensu") << "Server returned '" << rec << "'";
-
+		
 		lock();
 		nCommands = commands.size();
 		unlock();
--- src/ofxSensu.h
+++ src/ofxSensu.h
@@ -22,20 +22,20 @@
 #endif
 
 class ofxSensu : public ofThread{
-
+	
 public:
-
+	
 	enum Status{
 		RESOLVED = 0,
 		WARNING = 1,
 		CRITICAL_ERROR = 2
 	};
-
+	
 	~ofxSensu();
-
+	
 	void setup(string host = "127.0.0.1", int port = 3030);
 	bool isServerThere(); //ping! blocks!
-
+	
 	bool send(const string & alertName,
 			  const string & msg,
 			  Status status,
@@ -43,7 +43,7 @@ public:
 			  const string & filePath,
 			  bool blocking
 			  );
-
+	
 	bool send(const string & alertName,
 			  const string & msg,
 			  Status status,
@@ -51,34 +51,34 @@ public:
 			  const vector<string> & filePaths,
 			  bool blocking
 			  );
-
-  void sendStillAlive();
-  void buildStillAliveMessage(const string & name,
-                              const int ttl,
-                              const string & output,
-                              const string & remediation,
-                              const int occurrences,
-                              const int severity
-                              );
-
+	
+	void sendStillAlive();
+	void buildStillAliveMessage(const string & name,
+								const int ttl,
+								const string & output,
+								const string & remediation,
+								const int occurrences,
+								const int severity
+								);
+	
 protected:
-
+	
 	ofxTCPSettings settings = ofxTCPSettings("127.0.0.1", 3030);
-
+	
 	bool isSetup = false;
 	float reconnectWait = 10;
-
+	
 	string buildCommand(const string & alertName,
 						const string & msg,
 						Status status,
 						const vector<string> & emailAddresses,
 						const vector<string> & filePaths);
-
+	
 	void threadedFunction();
-
+	
 	bool connect(ofxTCPClient & client);
 	bool sendCmdBlocking(const string & cmd);
-
+	
 	vector<string> commands;
-    string stillAliveMsg;
+	string stillAliveMsg;
 };
